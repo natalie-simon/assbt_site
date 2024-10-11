a93a8d6037c0093232d33cd88477ad67a78402d2
natalie-simon <admin@nataliesimon.fr>
2024-10-11 14:38:48 +0200

    mise en place de la structure de la page

---
+++ src/components/BandeauAssbt.vue
@@ -0,0 +1,97 @@
+<script setup lang="ts">
+import Toolbar from 'primevue/toolbar';
+import Drawer from 'primevue/drawer';
+import InputText from 'primevue/inputtext';
+import IconField from 'primevue/iconfield';
+import { ref } from "vue";
+
+const visible = ref(false);
+const value = ref(null);
+</script>
+
+<template>
+  <div>
+    <div class="col-12 opacity80">
+      <Toolbar class="bg-assbt-primary">
+        <template #start>
+          <img src="../assets/images/logo.png" alt="logo" class="logo" />
+        </template>
+        <template #center>
+          <router-link class="bandeau-link text-white bold-24" to="/">Accueil</router-link>
+          <router-link class="bandeau-link text-white bold-24" to="/inscription">Inscription</router-link>
+          <router-link class="bandeau-link text-white bold-24" to="/contact">Contact</router-link>
+        </template>
+        <template #end>
+          <span class="pi pi-user" style="font-size: 2.5rem" @click="visible = true"></span>
+        </template>
+      </Toolbar>
+      <Drawer v-model:visible="visible" class="assbt-connexion bg-assbt-dark opacity80" position="right"
+        :showCloseIcon="false">
+        <div class="center-logo-x2">
+          <img src="@/assets/images/logo.png" alt="logo" class="logo-x2" />
+        </div>
+        <div class="bold-24 text-assbt-shine mt100">
+          CONNEXION
+        </div>
+        <div class="mt-5">
+          <div>Email</div>
+          <InputText class="input-assbt-login" id="email" v-model="value" placeholder="Entrer votre email" />
+        </div>
+        <div class="mt-5">
+          <div>Mot de passe</div>
+          <IconField>
+            <InputText class="input-assbt-login" id="password" v-model="value"
+              placeholder="Entrer votre mot de passe" />
+            <InputIcon>
+              <span class="pi pi-eye"></span>
+            </InputIcon>
+          </IconField>
+        </div>
+        <div class="mt-4 text-center">
+          <button class="btn bg-assbt-primary rounded-pill col-10 text-white">Connexion</button>
+        </div>
+        <div class="col-12 mt-3 text-center">
+          Mot de passe oublié ?
+        </div>
+        <template #footer>
+          <div class="text-center mb-5">Créer son compte</div>
+        </template>
+      </Drawer>
+    </div>
+  </div>
+</template>
+
+<style scoped>
+.logo {
+  width: 100px;
+  height: 64px;
+}
+
+.logo-x2 {
+  width: 200px;
+  height: 128px;
+}
+
+.bandeau-link {
+  padding-left: 17px;
+  padding-right: 17px;
+  text-decoration: none !important;
+}
+
+.center-logo-x2 {
+  display: flex;
+  justify-content: center;
+  margin-top: 15px;
+}
+
+.input-assbt-login {
+  background-color: inherit;
+  border-color: white;
+  border-bottom-style: solid;
+  border-top-style: none;
+  border-left-style: none;
+  border-right-style: none;
+  border-radius: inherit;
+  width: 90%;
+}
+</style>
